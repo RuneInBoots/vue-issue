79d39b9647939f68692d7e7bfc3d9f13cde894c2
rune <sara@trackuity.com>
2017-05-18 22:10:57 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <section id="app">
+    <h1>parent component</h1>
+    <h2>text inside parent</h2>
+    <p>parent sets color to crimson</p>
+    <child-component>
+      <p slot="foobar"> text from inside parent trough slot</p>
+    </child-component>
+   </section>
+  </section>
+</template>
+
+<script>
+import childComponent from './components/childComponent.vue';
+
+export default {
+  components: {childComponent}
+}
+</script>
+
+<style scoped>
+  p {color: crimson;}
+</style>
---
+++ src/components/childComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <transition>
+    <section>
+      <h1>Child component</h1>
+      <h2>text inside child</h2>
+      <p>child sets background to gold</p>
+      <h2>text from parent with slot</h2>
+      <slot name="foobar"></slot>
+    </section>
+  </transition>
+</template>
+
+<style lang="css" scoped>
+  p {background-color: gold;}
+</style>
